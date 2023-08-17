ae4c7f224de8e79bb982298c5f55f9042394adeb
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-17 15:34:13 +0300

    Recent Changes

---
+++ dz6/Program2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Program2;
+
+class Program2
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+class PlanetCatalog
+{
+    public List<Planet> PlanetsList { get; set; }
+    public PlanetCatalog()
+    {
+        PlanetsList = new List<Planet>(){new Planet("Венера", 123,123, new Planet())}
+    }
+    public GetPlanet(string name)
+    {
+
+    }
+
+}
+class Planet
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public int Length { get; set; }
+    public Planet Previous { get; set;}
+    public Planet(string name, int number, int length, Planet previous)
+    {
+        Name=name;
+        Number=number;
+        Length=length;
+        Previous=previous;
+    }
+}
+
