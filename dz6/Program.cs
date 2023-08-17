96b7a7e845fa068575d3a4665ce67242048c6834
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-17 12:11:34 +0300

    ready program 1

--- dz6/Program.cs
+++ dz6/Program.cs
@@ -8,6 +8,9 @@ class Program
         var planet2 = new { Name = "Земля", Number = 3, Length= 40075, Previous = planet1};
         var planet3 = new { Name = "Марс", Number = 4, Length= 21326, Previous = planet2};
         var planet4 = new { Name = "Венера", Number = 2, Length= 38025};
-        Console.WriteLine(planet1.ToString()+"Венера?: " + planet1==planet1.ToString());
+        Console.WriteLine(planet1+String.Format(" Венера?: {0}", planet1.Equals(planet1)));
+        Console.WriteLine(planet2+String.Format(" Венера?: {0}", planet2.Equals(planet1)));
+        Console.WriteLine(planet3+String.Format(" Венера?: {0}", planet3.Equals(planet1)));
+        Console.WriteLine(planet4+String.Format(" Венера?: {0}", planet4.Equals(planet1)));
     }
 }
