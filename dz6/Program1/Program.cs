b3271661a5f175f459d5c39e438c769c16b44cb6
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-21 09:49:52 +0300

    Fixed prev planet program1

--- dz6/Program1/Program.cs
+++ dz6/Program1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main()
     {
-        var planet1 = new { Name = "Венера", Number = 2, Length= 38025};
+        var planet1 = new { Name = "Венера", Number = 2, Length= 38025, Previous= null as object};
         var planet2 = new { Name = "Земля", Number = 3, Length= 40075, Previous = planet1};
         var planet3 = new { Name = "Марс", Number = 4, Length= 21326, Previous = planet2};
-        var planet4 = new { Name = "Венера", Number = 2, Length= 38025};
+        var planet4 = new { Name = "Венера", Number = 2, Length= 38025, Previous= null as object};
         Console.WriteLine(planet1+String.Format(" Венера?: {0}", planet1.Equals(planet1)));
         Console.WriteLine(planet2+String.Format(" Венера?: {0}", planet2.Equals(planet1)));
         Console.WriteLine(planet3+String.Format(" Венера?: {0}", planet3.Equals(planet1)));
