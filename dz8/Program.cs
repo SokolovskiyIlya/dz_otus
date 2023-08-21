ab6d3fdc23b44ebc37e4c19c83df9e0454fc7749
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-21 17:56:21 +0300

    init Dz 8

---
+++ dz8/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace dz8;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            var name = Console.ReadLine();
+            var salary = double.Parse(Console.ReadLine());
+            Worker root = null;
+            if (String.IsNullOrEmpty(name))
+            {
+                break;
+            }
+            if (root == null)
+            {
+                root = new Worker()
+                {
+                    Salary = salary,
+                    Name = name
+                };
+            }
+            else
+            {
+                AddNode(root, new Worker
+                {
+                    Salary = salary,
+                    Name = name
+                });
+            }
+        }
+    }
+
+}
+public class Worker
+{
+    public double Salary { get; set; };
+    public string Name { get; set; };
+    public Worker Left { get; set; };
+    public Worker Right { get; set; };
+}
