e453a117e2a6620695f0cf69280872641627d521
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-23 10:03:35 +0300

    Init 2

--- dz8/dz8/Program.cs
+++ dz8/dz8/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main()
     {
+        Worker root = null;
         while (true)
         {
             var name = Console.ReadLine();
             var salary = double.Parse(Console.ReadLine());
-            Worker root = null;
             if (String.IsNullOrEmpty(name))
             {
                 break;
@@ -31,12 +31,37 @@ class Program
             }
         }
     }
+    static void AddNode(Worker root, Worker toAdd)
+    {
+        if (toAdd.Salary < root.Salary)
+        {
+            if (root.Left != null)
+            {
+                AddNode(root.Left, toAdd);
+            }
+            else
+            {
+                root.Left = toAdd;
+            }
+        }
+        else
+        {
 
+            if (root.Right != null)
+            {
+                AddNode(root.Right, toAdd);
+            }
+            else
+            {
+                root.Right = toAdd;
+            }
+        }
+    }
 }
 public class Worker
 {
-    public double Salary { get; set; };
-    public string Name { get; set; };
-    public Worker Left { get; set; };
-    public Worker Right { get; set; };
+    public double Salary { get; set; }
+    public string Name { get; set; }
+    public Worker Left { get; set; }
+    public Worker Right { get; set; }
 }
