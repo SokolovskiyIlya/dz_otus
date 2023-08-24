7b1f8b8185c486f25e969181d16de9f72ee5926c
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-24 11:53:05 +0300

    1-2 tasks

--- dz9/ImageDownloader.cs
+++ dz9/ImageDownloader.cs
@@ -1,6 +1,13 @@
-﻿namespace dz9;
+﻿using System.Net;
+namespace dz9;
 
 public class ImageDownloader
 {
-
+    public static void Download(string remoteUrl, string fileName)
+    {
+		var myWebClient = new WebClient();
+		Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUrl);
+		myWebClient.DownloadFile(remoteUrl, fileName);
+		Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUrl);	
+    }
 }
--- dz9/Program.cs
+++ dz9/Program.cs
@@ -4,6 +4,10 @@
     {
         private static void Main()
         {
+            ImageDownloader imgDwn = new ImageDownloader();
+            ImageDownloader.Download("https://proprikol.ru/wp-content/uploads/2020/06/krasivye-kartinki-zvezd-10.jpg", "space.jpg");
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
         }
     }
 }
