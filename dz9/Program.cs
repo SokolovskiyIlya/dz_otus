8cfdac7ee111348aadc48ed532c60efdf5ba7f76
IlyaSokolovskkiy <sokolguitarman@gmail.com>
2023-08-24 15:01:12 +0300

    recent changes

--- dz9/Program.cs
+++ dz9/Program.cs
@@ -5,7 +5,7 @@
         private static void Main()
         {
             ImageDownloader imgDwn = new ImageDownloader();
-            ImageDownloader.Download("https://proprikol.ru/wp-content/uploads/2020/06/krasivye-kartinki-zvezd-10.jpg", "space.jpg");
+            ImageDownloader.Download("https://img3.akspic.ru/crops/8/9/7/4/7/174798/174798-harlej_devidson-motocikl-harli-harley_davidson_sportster-harley_davidson_fat_boy-7680x4320.jpg", "space.jpg");
             Console.WriteLine("Нажмите любую клавишу для выхода");
             Console.ReadKey();
         }
