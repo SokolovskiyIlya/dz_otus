0537f322ba172e591c7197ede2829770e385ca61
Соколовский Илья Игоревич <ISokolovskiy@ibs.root.ibscorp.ru>
2023-08-25 11:49:13 +0300

    ready task 3

--- dz9/ImageDownloader.cs
+++ dz9/ImageDownloader.cs
@@ -3,11 +3,15 @@ namespace dz9;
 
 public class ImageDownloader
 {
-    public static void Download(string remoteUrl, string fileName)
+	public event Action<string>? ImageStarted;
+	public event Action<string>? ImageCompleted;
+    public void Download(string remoteUrl, string fileName)
     {
+		ImageStarted?.Invoke(fileName);
 		var myWebClient = new WebClient();
 		Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUrl);
 		myWebClient.DownloadFile(remoteUrl, fileName);
-		Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUrl);	
+		Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUrl);
+		ImageCompleted?.Invoke(fileName);	
     }
 }
--- dz9/Program.cs
+++ dz9/Program.cs
@@ -5,9 +5,16 @@
         private static void Main()
         {
             ImageDownloader imgDwn = new ImageDownloader();
-            ImageDownloader.Download("https://img3.akspic.ru/crops/8/9/7/4/7/174798/174798-harlej_devidson-motocikl-harli-harley_davidson_sportster-harley_davidson_fat_boy-7680x4320.jpg", "space.jpg");
+            var start = (string fileName) => Console.WriteLine($"Скачивание файла {fileName} началось");
+            var complete = (string fileName) => Console.WriteLine($"Скачивание файла {fileName} звершилось");
+            imgDwn.ImageStarted += start;
+            imgDwn.ImageCompleted += complete;
+            imgDwn.Download("https://img3.akspic.ru/crops/8/9/7/4/7/174798/174798-harlej_devidson-motocikl-harli-harley_davidson_sportster-harley_davidson_fat_boy-7680x4320.jpg", "space.jpg");
+            imgDwn.ImageStarted -= start;
+            imgDwn.ImageCompleted -= complete;
             Console.WriteLine("Нажмите любую клавишу для выхода");
             Console.ReadKey();
         }
+        
     }
 }
