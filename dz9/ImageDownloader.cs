cf1ecda316c3adfa48017fc301534825f6688812
Соколовский Илья Игоревич <sokolguitarman@gmail.com>
2023-08-28 15:15:35 +0300

    Ready task 4

--- dz9/ImageDownloader.cs
+++ dz9/ImageDownloader.cs
@@ -5,12 +5,12 @@ public class ImageDownloader
 {
 	public event Action<string>? ImageStarted;
 	public event Action<string>? ImageCompleted;
-    public void Download(string remoteUrl, string fileName)
+    public async Task DownloadAsync(string remoteUrl, string fileName)
     {
 		ImageStarted?.Invoke(fileName);
 		var myWebClient = new WebClient();
 		Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUrl);
-		myWebClient.DownloadFile(remoteUrl, fileName);
+		await myWebClient.DownloadFileTaskAsync(remoteUrl, fileName);
 		Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUrl);
 		ImageCompleted?.Invoke(fileName);	
     }
--- dz9/Program.cs
+++ dz9/Program.cs
@@ -6,15 +6,25 @@
         {
             ImageDownloader imgDwn = new ImageDownloader();
             var start = (string fileName) => Console.WriteLine($"Скачивание файла {fileName} началось");
-            var complete = (string fileName) => Console.WriteLine($"Скачивание файла {fileName} звершилось");
+            var complete = (string fileName) => Console.WriteLine($"Скачивание файла {fileName} завершилось");
             imgDwn.ImageStarted += start;
             imgDwn.ImageCompleted += complete;
-            imgDwn.Download("https://img3.akspic.ru/crops/8/9/7/4/7/174798/174798-harlej_devidson-motocikl-harli-harley_davidson_sportster-harley_davidson_fat_boy-7680x4320.jpg", "space.jpg");
+            Task download = imgDwn.DownloadAsync("https://img3.akspic.ru/crops/8/9/7/4/7/174798/174798-harlej_devidson-motocikl-harli-harley_davidson_sportster-harley_davidson_fat_boy-7680x4320.jpg", "space.jpg");
+            Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.KeyChar == 'a' || key.KeyChar == 'а')
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("Загрузка завершена? - {0}", download.IsCompleted));
+                }
+            }
             imgDwn.ImageStarted -= start;
             imgDwn.ImageCompleted -= complete;
-            Console.WriteLine("Нажмите любую клавишу для выхода");
-            Console.ReadKey();
         }
-        
     }
 }
